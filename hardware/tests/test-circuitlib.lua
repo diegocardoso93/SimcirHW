a99997d8abf8c2f4117f0b2a8d1371be4ca06b62
dieg0 <dieg0card0s0@hotmail.com>
2017-11-30 13:16:52 -0200

    test circuitlib with asserteq

--- hardware/tests/test-circuitlib.lua
+++ hardware/tests/test-circuitlib.lua
@@ -1,10 +1,22 @@
 
 require "../circuitlib"
+local test = require "../pl/test"
+local asserteq = test.asserteq
 
-print(0 -bitor- 1)
-print(1 -bitand- 1)
-print(1 -bitxor- 0)
-print(1 -bitor- 0)
-print(0 -bitnand- 0)
+asserteq(bitnot(0),     1)
+asserteq(0 -bitor- 1,   1)
+asserteq(1 -bitand- 1,  1)
+asserteq(1 -bitxor- 0,  1)
+asserteq(0 -bitnand- 0, 1)
 
-print(convert_circuit("(A AND B) OR (NOT(B) XOR A)"))
+asserteq(bitnot(1),     0)
+asserteq(0 -bitor- 0,   0)
+asserteq(1 -bitand- 0,  0)
+asserteq(1 -bitxor- 1,  0)
+asserteq(1 -bitnand- 1, 0)
+
+ckt = convert_circuit("(A OR B) AND NOT(B XOR NOT(A))")
+asserteq(ckt, '(A -bitor- B) -bitand- bitnot(B -bitxor- bitnot(A))')
+
+A = 0; B = 1
+asserteq((A -bitor- B) -bitand- bitnot((B -bitxor- bitnot(A))), 1)
