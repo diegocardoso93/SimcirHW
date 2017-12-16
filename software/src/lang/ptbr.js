738aad997c77c9cbf112f0504aac30aa846568cf
dieg0 <dieg0card0s0@hotmail.com>
2017-12-16 14:19:00 -0200

    labels for lang pt-br

---
+++ software/src/lang/ptbr.js
@@ -0,0 +1,8 @@
+
+var LANG = {
+  "ERR_OUT_FAULT": "Você precisa adicionar ao menos uma saída.",
+  "ERR_INVALID_CIRCUIT": "Circuito com má formação.",
+  "SUC_ENVIADO": "Enviado com sucesso.",
+  "SUC_VALID_CIRCUIT": "Circuito válido.",
+  "INF_SOME": "xxx"
+}
