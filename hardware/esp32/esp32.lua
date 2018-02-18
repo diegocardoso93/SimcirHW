c3f63bc86ab269580bcf7c2325c246561443aac4
dieg0 <dieg0card0s0@hotmail.com>
2018-02-18 12:25:24 -0300

    image and pins config esp32s

--- hardware/esp32/esp32.lua
+++ hardware/esp32/esp32.lua
@@ -3,19 +3,32 @@ local Esp32 = {}
 
 Esp32.NodeMCU_ESP32S = {
   named_pins = {
-    D0  =  19, -- GPIO16
-    D1  =  21, -- GPIO5
-    D2  =  2, -- GPIO4
-    D3  =  3, -- GPIO0
-    D4  =  4, -- GPIO2
-    D5  =  5, -- GPIO14
-    D6  =  6, -- GPIO12
-    D7  =  7, -- GPIO13
-    D8  =  8, -- GPIO15
-    -- RX  =  9, -- GPIO3
-    -- TX  = 10, -- GPIO1
-    -- SD2 = 11, -- GPIO9
-    SD3 = 12  -- GPIO10
+    VP  = 36, -- GPIO36
+    VN  = 39, -- GPIO39
+    D34 = 34, -- GPIO34
+    D35 = 35, -- GPIO35
+    D32 = 32, -- GPIO32
+    D33 = 33, -- GPIO33
+    D25 = 25, -- GPIO25
+    D26 = 26, -- GPIO26
+    D27 = 27, -- GPIO27
+    D14 = 14, -- GPIO14
+    D12 = 12, -- GPIO12
+    D13 = 13, -- GPIO13
+    
+    D15 = 15, -- GPIO15
+    D2  =  2, -- GPIO2 (builtin led)
+    D4  =  4, -- GPIO4
+    RX2 = 16, -- GPIO16
+    TX2 = 17, -- GPIO17
+    D5  =  5, -- GPIO5
+    D18 = 18, -- GPIO18
+    D19 = 19, -- GPIO19
+    D21 = 21, -- GPIO21
+    RX0 =  3, -- GPIO3
+    TX0 =  1, -- GPIO1
+    D23 = 22, -- GPIO22
+    D22 = 23  -- GPIO23
   }
 }
 
